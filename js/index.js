389c4505890dfbdf5da5c8d7c13d067baec515da
Alexis <alexisdavalos.tech@gmail.com>
2019-12-03 16:23:34 -0500

    Fun Bus Img Click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+//1. Fun Bus Img Click
+let funBusImg = document.querySelector('header img');
+funBusImg.addEventListener("click", () => {
+    funBusImg.style.transform = "scale(1.2)";
+    funBusImg.style.transition = "transform 0.3s"
+    funBusImg.style.zIndex = "-1";
+    funBusImg.addEventListener("click", () => {
+        funBusImg.style.transform = "scale(1)";
+    })
+})
